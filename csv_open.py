f5c627f7d597c374a0f788535f4cdbb8a12c6257
CarinaHe <carinaheiling@gmx.at>
2021-08-11 08:34:47 +0200

    commit file

--- csv_open.py
+++ csv_open.py
@@ -4,3 +4,9 @@ with open("dmv.csv") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
         print(' , '.join(row))
+
+
+# andere möglichkeit:
+# fhand = open("dmv.csv")
+# for x in fhand:
+#     print(x)
